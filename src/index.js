8dbdafd7ce72ba692e12699502c835958f763de0
Luis Mendoza <lmendoza.mg@gmail.com>
2023-08-24 21:14:36 -0400

    feat: add nes.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import "nes.css/css/nes.min.css";
 import './index.css';
+
 import { App } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
